1f833cf010f8a6add8769124086b8bbb2d6a0d3e
Francis Chung <francis.c.chung@gmail.com>
2024-12-30 15:17:25 +1100

    Flesh out NewTodoForm with new Button that uses Server Actions

--- intro-v3-app/components/NewTodoForm.tsx
+++ intro-v3-app/components/NewTodoForm.tsx
@@ -1,7 +1,10 @@
+import {newTodo} from '@/utils/actions'
+
 const NewTodoForm = () => {
     return <div>
-        <form>
-            <input type="text" />
+        <form action={newTodo}>
+            <input name="content" type="text" className="border border-black/25" />
+            <button type="submit">New Todo</button>
         </form>
     </div>
 }
