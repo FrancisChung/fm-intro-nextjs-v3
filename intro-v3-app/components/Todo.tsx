e87b32476e61d927156c62fc4f6a9978d4c98ac2
Francis Chung <francis.c.chung@gmail.com>
2025-01-01 17:44:09 +1100

    Server Action Mutation with startTransition demo

--- intro-v3-app/components/Todo.tsx
+++ intro-v3-app/components/Todo.tsx
@@ -1,6 +1,18 @@
 // @ts-ignore
-const Todo = ({todo}) => {
-    return <div>{todo.content}</div>
-}
+'use client'
+import {useTransition} from "react";
+import { completeTodo} from "@/utils/actions";
 
+const Todo = ({todo}) => {
+    const [isPending, startTransition] = useTransition()
+    return (
+        <div
+            onClick={() => startTransition(()=> completeTodo(todo.id))}
+            className = {` border border-white/20 cursor-pointer ${todo.completed ? 'line-through text-white-900' : '' }`}>
+            {todo.content}
+        </div>
+    )
+}
+// className = {` border border-white/20 cursor-pointer ${todo.completed ? 'line-through text-white-900' : '' }`}
+// onClick={() => startTransition(()=> completeTodo(todo.id))} >
 export default Todo
