61e41238f2538b100b1106a87e9e7c1d96aee3f0
Francis Chung <francis.c.chung@gmail.com>
2025-01-02 11:56:58 +1100

    POST API route example

--- intro-v3-app/app/api/todos/route.ts
+++ intro-v3-app/app/api/todos/route.ts
@@ -1,5 +1,10 @@
 import {NextResponse} from "next/server";
 
 export const GET = async (request: Request) => {
-    return NextResponse.json({message: 'hello matey'})
+    return NextResponse.json({message: 'hello GET matey'})
+}
+
+export const POST = async (request: Request) => {
+    const data = await request.body;
+    return NextResponse.json({message: 'hello POST matey {data}'})
 }
